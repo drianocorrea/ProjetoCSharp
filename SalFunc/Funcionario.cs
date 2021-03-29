98a70306e43ad1e67d66af0be040387dc7249d50
drianocorrea <drianocorrea@gmail.com>
2021-03-28 22:25:36 -0300

    Adicionando calculo de salario de funcionario

---
+++ SalFunc/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalarioFuncionario
+{
+    class Funcionario
+    {
+        private int matricula, horasTrabalhadas;
+        private double valorSalario, salarioFinal;
+
+		public int Matricula { get => matricula; set => matricula = value; }
+		public int HorasTrabalhadas { get => horasTrabalhadas; set => horasTrabalhadas = value; }
+		public double ValorSalario { get => valorSalario; set => valorSalario = value; }
+		public double SalarioFinal { get => salarioFinal; set => salarioFinal = value; }
+
+		public void calculaSalarioFinal() 
+		{
+			SalarioFinal = ValorSalario * HorasTrabalhadas;
+		}
+	}
+}
--- SalFunc/SalFunc.cs
+++ SalFunc/SalFunc.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿/*Exercícios de fixação –entregar: 29-03-2021
+1 ) Fazer um programa que leia o número de matrícula de um funcionário, quantidade de horas trabalhadas no
+mês, o valor que recebe por hora e calcule o salário desse funcionário. A seguir, mostre o número de matrícula
+e o salário total do funcionário (com duas casas decimais). {base para pensar em algo mais elaborado, que
+pode ser desenhado como parte de um sistema para implantar para uma pequena empresa gerar relatórios
+mensais para o contador}
+*/
+
+using System;
 
 namespace SalarioFuncionario
 {
-	class Program
+	class SalFunc
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			bool continua = false;
+
+			Funcionario funcionario;
+
+			while (!continua) {
+				funcionario = new Funcionario();
+
+				try
+				{
+					Console.WriteLine("Digitar matrícula: ");
+					funcionario.Matricula = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine("Digitar quantidade de horas trabalhadas: ");
+					funcionario.HorasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine("Digitar valor do sálario por hora: ");
+					funcionario.ValorSalario = Convert.ToDouble(Console.ReadLine());
+
+					funcionario.calculaSalarioFinal();
+
+					Console.WriteLine($"\nO funcionário da matrícula {funcionario.Matricula} recebeu um total " +
+									  $"de R$ {funcionario.SalarioFinal} por {funcionario.HorasTrabalhadas}H\n");
+				}
+				catch (Exception ex) 
+				{
+					Console.WriteLine("\nAlgum dado não foi digitado corretamente \n");
+				}
+
+				Console.WriteLine("Deseja consultar de outro funcionário (Y - sim, N - não): ");
+				continua = Console.ReadLine().Equals("Y", StringComparison.OrdinalIgnoreCase) ? false : true;
+			}
 		}
 	}
 }
